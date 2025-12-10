34da4d701ab8c1e5dcd16a583e982256aee93888
masonnguyen1243 <meowjcisgod@gmail.com>
2025-12-10 14:59:30 +0700

    feat: authentication with clerk

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import SearchBar from "./SearchBar";
 import { Bell, Home } from "lucide-react";
 import ShoppingCartIcon from "./ShoppingCartIcon";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import ProfileButton from "@/components/ProfileButton";
 
 const Navbar = () => {
   return (
@@ -30,7 +32,12 @@ const Navbar = () => {
         <Bell className="w-4 h-4 text-gray-600 cursor-pointer" />
         <ShoppingCartIcon />
 
-        <Link href={"/login"}>Sign in</Link>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <ProfileButton />
+        </SignedIn>
       </div>
     </div>
   );
