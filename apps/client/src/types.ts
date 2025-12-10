aae2500d6599cf3de660552d5a768a8d0502438e
masonnguyen1243 <meowjcisgod@gmail.com>
2025-12-10 13:34:55 +0700

    feat: add project to monorepo

---
+++ apps/client/src/types.ts
@@ -0,0 +1,69 @@
+import { z } from "zod";
+
+// Product Type
+export type ProductType = {
+  id: string | number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  sizes: [string, ...string[]];
+  colors: [string, ...string[]];
+  images: Record<string, string>;
+  //   images: { [color: string]: string };
+};
+
+export type ProductsType = ProductType[];
+
+// Cart Item Type
+export type CartItemType = ProductType & {
+  quantity: number;
+  selectedSize: string;
+  selectedColor: string;
+};
+
+export type CartItemsType = CartItemType[];
+
+// Shipping Form Schema and Type
+export const shippingFormSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email().min(1, "Email is required"),
+  phone: z
+    .string()
+    .min(7, "Invalid phone number")
+    .max(11, "Invalid phone number")
+    .regex(/^\d+$/, "Invalid phone number"),
+  address: z.string().min(1, "Address is required"),
+  city: z.string().min(1, "City is required"),
+});
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+// Payment Form Schema and Type
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required"),
+  cardNumber: z
+    .string()
+    .min(16, "Card number must be 16 digits")
+    .max(16, "Card number must be 16 digits"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
+      "Expiration date must be in MM/YY format"
+    ),
+  cvv: z.string().min(3, "CVV is required").max(3, "CVV must be 3 digits"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
+
+export type CartStoreStateType = {
+  cart: CartItemsType;
+  hasHydrated: boolean;
+};
+
+export type CartStoreActionsType = {
+  addToCart: (product: CartItemType) => void;
+  removeFromCart: (product: CartItemType) => void;
+  clearCart: () => void;
+};
