082acde4348a871f61773feee2c0037953aeedc9
masonnguyen1243 <meowjcisgod@gmail.com>
2025-12-16 16:42:23 +0700

    feat: fetch user orders

--- apps/client/src/app/orders/page.tsx
+++ apps/client/src/app/orders/page.tsx
@@ -1,4 +1,73 @@
-const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+import { auth } from "@clerk/nextjs/server";
+import { OrderType } from "@repo/types";
+
+const fetchOrders = async () => {
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_ORDER_SERVICE_URL}/user-orders`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const data: OrderType[] = await res.json();
+  return data;
+};
+
+const OrdersPage = async () => {
+  const orders = await fetchOrders();
+
+  if (!orders) {
+    return <div>No orders found!</div>;
+  }
+
+  console.log(`User orders`, orders);
+
+  return (
+    <div className="">
+      <div className="text-2xl my-4 font-medium">Orders</div>
+      <ul>
+        {orders.map((order) => (
+          <li key={order._id} className="flex items-center mb-4">
+            <div className="w-1/4">
+              <span className="font-medium text-sm text-gray-500">
+                Order Id
+              </span>
+              <p>{order._id}</p>
+            </div>
+            <div className="w-1/12">
+              <span className="font-medium text-sm text-gray-500">Total</span>
+              <p>{order.amount / 100}</p>
+            </div>
+            <div className="w-1/12">
+              <span className="font-medium text-sm text-gray-500">Status</span>
+              <p>{order.status}</p>
+            </div>
+            <div className="w-1/8">
+              <span className="font-medium text-sm text-gray-500">Date</span>
+              <p>
+                {order.createdAt
+                  ? new Date(order.createdAt).toLocaleDateString("en-US")
+                  : "-"}
+              </p>
+            </div>
+            <div className="">
+              <span className="font-medium text-sm text-gray-500">
+                Products
+              </span>
+              <p>
+                {order.products?.map((product) => product.name).join(", ") ||
+                  "-"}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default OrdersPage;
