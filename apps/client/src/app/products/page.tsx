c2b96d2952b85ad1d8c7de63414504d3b92756cc
masonnguyen1243 <meowjcisgod@gmail.com>
2025-12-16 14:23:12 +0700

    feat: fetch products

--- apps/client/src/app/products/page.tsx
+++ apps/client/src/app/products/page.tsx
@@ -3,13 +3,20 @@ import ProductList from "@/components/ProductList";
 const ProductsPage = async ({
   searchParams,
 }: {
-  searchParams: Promise<{ category: string }>;
+  searchParams: Promise<{ category: string; sort: string; search: string }>;
 }) => {
   const category = (await searchParams).category || "";
+  const sort = (await searchParams).sort || "";
+  const search = (await searchParams).search || "";
 
   return (
     <div className="">
-      <ProductList category={category} params="products" />
+      <ProductList
+        category={category}
+        params="products"
+        sort={sort}
+        search={search}
+      />
     </div>
   );
 };
