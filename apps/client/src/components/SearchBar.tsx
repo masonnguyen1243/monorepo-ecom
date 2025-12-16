e9fe8a21e1dbf28da667819223c0261aa73fdfa7
masonnguyen1243 <meowjcisgod@gmail.com>
2025-12-16 16:12:19 +0700

    feat: search filter

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [value, setValue] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleSearch = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("search", value);
+    router.push(`/products?${params.toString()}`, { scroll: false });
+  };
+
   return (
     <div className="hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 shadow-md">
       <Search className="w-4 h-4 text-gray-500" />
@@ -9,6 +23,12 @@ const SearchBar = () => {
         placeholder="Search..."
         type="text"
         className="text-sm outline-0"
+        onChange={(c) => setValue(c.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch(value);
+          }
+        }}
       />
     </div>
   );
