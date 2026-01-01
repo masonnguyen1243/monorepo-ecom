88195ae852eef3937658c01939f753949d27123f
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-01 14:54:26 +0700

    feat: create product

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,7 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
