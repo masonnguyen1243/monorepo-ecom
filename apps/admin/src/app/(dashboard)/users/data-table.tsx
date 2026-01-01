8229064d74229aad2bc276db0b7ee3b34b6548ed
masonnguyen1243 <meowjcisgod@gmail.com>
2026-01-01 13:56:37 +0700

    feat: delete user

--- apps/admin/src/app/(dashboard)/users/data-table.tsx
+++ apps/admin/src/app/(dashboard)/users/data-table.tsx
@@ -21,6 +21,11 @@ import {
 import { DataTablePagination } from "@/components/TablePagination";
 import { useState } from "react";
 import { Trash2 } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { User } from "@clerk/nextjs/server";
+import { useRouter } from "next/navigation";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -48,14 +53,52 @@ export function DataTable<TData, TValue>({
     },
   });
 
+  const { getToken } = useAuth();
+  const router = useRouter();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      const selectedRows = table.getSelectedRowModel().rows;
+
+      Promise.all(
+        selectedRows.map(async (row) => {
+          const userId = (row.original as User).id;
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_USER_SERVICE_URL}/users/${userId}`,
+            {
+              method: "DELETE",
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+        })
+      );
+    },
+    onSuccess: () => {
+      toast.success("User deleted successfully!");
+      router.refresh();
+    },
+    onError: (error) => {
+      console.log(error);
+
+      toast.error(error.message);
+    },
+  });
+
   console.log(rowSelection);
   return (
     <div className="rounded-md border">
       {Object.keys(rowSelection).length > 0 && (
         <div className="flex justify-end">
-          <button className="flex items-center gap-2 bg-red-500 text-white px-2 py-1 text-sm rounded-md m-4 cursor-pointer">
+          <button
+            onClick={() => mutation.mutate()}
+            disabled={mutation.isPending}
+            className="flex items-center gap-2 bg-red-500 text-white px-2 py-1 text-sm rounded-md m-4 cursor-pointer"
+          >
             <Trash2 className="w-4 h-4" />
-            Delete User(s)
+            {mutation.isPending ? "Deleting" : "Delete User(s)"}
           </button>
         </div>
       )}
